5af2d95564fb78cff546a7d5b4e80cc783e44c5b
matej <matej@Martins-MBP.petrak.sk>
2018-09-23 11:09:45 +0200

    adding du_2

---
+++ bitove_operacie/du_2/main.cpp
@@ -0,0 +1,20 @@
+//Matej Las, 23.9.2018
+//
+#include <iostream>
+
+int main() {
+  int cislo_1, cislo_2;
+  std::cout << "Zadaj prve cislo: ";
+  std::cin >> cislo_1;
+  std::cout << "Zadaj druhe cislo: ";
+  std::cin >> cislo_2;
+
+  if ((cislo_1 & 1) == (cislo_2 & 1)) {
+    std::cout << "Zadane cisla maju posledny bit rovnaky.\n";
+  }
+  else {
+    std::cout << "Zadane cisla maju posledny bit rozdielny.\n";
+  }
+
+  return 0;
+}
