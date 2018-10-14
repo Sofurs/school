2f8fec968d1d9e0a34e0189ee99adb7a00022d44
Sofurs <matej@las.sk>
2018-10-14 14:34:14 +0100

    bug fix

--- viacrozmerne_pole/du_3/main.cpp
+++ viacrozmerne_pole/du_3/main.cpp
@@ -1,53 +1,73 @@
 #include <iostream>
 #include <iomanip>
 
-#define MAX_X 21
-#define MAX_Y 11
+#define MAX_X 10
+#define MAX_Y 10
+
+#define NUM_OF_ELEMENTS MAX_X *MAX_Y
 
 typedef int t_Array[MAX_X][MAX_Y];
 
-void fillArray(t_Array &arr) {
-    for(int i = 0; i < MAX_X; i++) {
-        for(int j = 0; j < MAX_Y; j++) {
+void fillArray(t_Array &arr)
+{
+    for (int i = 0; i < MAX_X; i++)
+    {
+        for (int j = 0; j < MAX_Y; j++)
+        {
             arr[i][j] = 0;
         }
     }
 }
 
-void fillSpiral(t_Array &arr) {
+void fillSpiral(t_Array &arr)
+{
     int count = 1;
     int depth = 0;
 
+    while (count <= NUM_OF_ELEMENTS)
+    {
 
-    while(depth < MAX_X / 2) {
-
-        for(int i = depth; i < MAX_Y - 1 - depth; i++, count++) {
+        for (int i = depth; i < MAX_Y - 1 - depth && count <= NUM_OF_ELEMENTS; i++, count++)
+        {
             arr[depth][i] = count;
         }
-        for (int i = depth; i < MAX_X - 1 - depth; i++, count++) {
+        for (int i = depth; i < MAX_X - 1 - depth && count <= NUM_OF_ELEMENTS; i++, count++)
+        {
             arr[i][MAX_Y - depth - 1] = count;
         }
-        for(int i = MAX_Y - depth - 1; i > 0 + depth ; i--, count++) {
+        for (int i = MAX_Y - depth - 1; i > 0 + depth && count <= NUM_OF_ELEMENTS; i--, count++)
+        {
             arr[MAX_X - depth - 1][i] = count;
         }
-        for(int i = MAX_X - depth - 1; i > 0 + depth; i--, count++) {
+        for (int i = MAX_X - depth - 1; i > 0 + depth && count <= NUM_OF_ELEMENTS; i--, count++)
+        {
             arr[i][depth] = count;
         }
 
-        depth++;  
+        if (MAX_X / 2 == depth && MAX_Y / 2 == depth)
+        {
+            arr[depth][depth] = count;
+            count++;
+        }
+
+        depth++;
     }
 }
 
-void printArray(t_Array &arr) {
-    for(int i = 0; i < MAX_X; i++) {
-        for(int j = 0; j < MAX_Y; j++) {
+void printArray(t_Array &arr)
+{
+    for (int i = 0; i < MAX_X; i++)
+    {
+        for (int j = 0; j < MAX_Y; j++)
+        {
             std::cout << std::setw(3) << arr[i][j] << " ";
         }
         std::cout << '\n';
     }
 }
 
-int main() {
+int main()
+{
 
     t_Array spiral;
 
