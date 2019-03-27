53254510c8345c6dce0d6cffd297a2ddeff5e373
Sofurs <matej@las.sk>
2019-03-27 15:30:44 +0100

    adding htmlParser to txt files

---
+++ htmlParser/main.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+
+int main() {
+    std::fstream inFile;
+    std::fstream tagFile;
+    std::fstream textFile;
+
+    inFile.open("html.txt", std::ios_base::in);
+    tagFile.open("tag.txt", std::ios_base::out | std::ios_base::trunc);
+    textFile.open("text.txt", std::ios_base::out | std::ios_base::trunc);
+
+    char temp;
+    char option;
+
+    while (!inFile.eof()) {
+        inFile >> temp;
+
+        if (temp == '<')
+            option = 't';  // tag
+        else if (temp == '>') {
+            tagFile << temp;
+            option = 'c';  // content
+            continue;
+        }
+
+        if (option == 't')
+            tagFile << temp;
+        else if (option == 'c')
+            textFile << temp;
+    }
+
+    return 0;
+}
